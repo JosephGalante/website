7dd9ab75a1c8b4bde8b9f76fd2acfee63e6bf1ef
Joe Galante <joe.galante@canopycapital.io>
2026-03-26 12:39:12 -0500

    update headshot

--- components/tilt-image.tsx
+++ components/tilt-image.tsx
@@ -3,8 +3,12 @@
 import Image from 'next/image'
 import { useState } from 'react'
 
+const headshotLink = '/headshot.jpeg'
+
 export function TiltImage() {
-  const [transform, setTransform] = useState('perspective(900px) rotateX(0deg) rotateY(0deg)')
+  const [transform, setTransform] = useState(
+    'perspective(900px) rotateX(0deg) rotateY(0deg)',
+  )
 
   const handleMove = (event: React.MouseEvent<HTMLDivElement>) => {
     const rect = event.currentTarget.getBoundingClientRect()
@@ -12,17 +16,23 @@ export function TiltImage() {
     const y = event.clientY - rect.top
     const rotateY = ((x / rect.width) * 10 - 5).toFixed(2)
     const rotateX = (5 - (y / rect.height) * 10).toFixed(2)
-    setTransform(`perspective(900px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`)
+    setTransform(
+      `perspective(900px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`,
+    )
   }
 
   const reset = () =>
     setTransform('perspective(900px) rotateX(0deg) rotateY(0deg)')
 
   return (
-    <div className="portrait-shell" onMouseMove={handleMove} onMouseLeave={reset}>
+    <div
+      className="portrait-shell"
+      onMouseMove={handleMove}
+      onMouseLeave={reset}
+    >
       <div className="portrait-frame" style={{ transform }}>
         <Image
-          src="/headshot_cropped.jpg"
+          src={headshotLink}
           alt="Joe Galante profile picture"
           width={440}
           height={520}
